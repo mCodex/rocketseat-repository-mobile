9c7ad682e9b8ff619bc15e3bb13eb142dfa5b5d8
Mateus Andrade <mat.andrade@live.com>
2020-02-24 13:53:13 -0300

    added flatlist and initial logic

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,46 @@
-import React, { memo } from 'react';
-
+import React, { memo, useState } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import api from '~/services/api';
+
+import {
+    Container,
+    Form,
+    Input,
+    SubmitButton,
+    List,
+    User,
+    Name,
+    Avatar,
+    Bio,
+    ProfileButton,
+    ProfileButtonText,
+} from './styles';
 
 const Main = () => {
+    const [newUser, setNewUser] = useState('');
+    const [users, setUsers] = useState([]);
+
+    const handleSubmit = async () => {
+        try {
+            const response = await api.get(`/users/${newUser}`);
+
+            const data = {
+                name: response.data.name,
+                login: response.data.login,
+                bio: response.data.bio,
+                avatar: response.data.avatar_url,
+            };
+
+            setUsers(prevState => [...prevState, data]);
+            setNewUser('');
+            Keyboard.dismiss();
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
+
     return (
         <Container>
             <Form>
@@ -12,11 +48,30 @@ const Main = () => {
                     autoCorrect={false}
                     autoCapitalize="none"
                     placeholder="Adicionar Usuário"
+                    value={newUser}
+                    onChangeText={setNewUser}
+                    returnKeyType="send"
+                    onSubmitEditing={handleSubmit}
                 />
-                <SubmitButton>
+                <SubmitButton onPress={handleSubmit}>
                     <Icon name="add" size={20} color="#fff" />
                 </SubmitButton>
             </Form>
+
+            <List
+                data={users}
+                keyExtractor={user => user.login}
+                renderItem={({ item }) => (
+                    <User>
+                        <Avatar source={{ uri: item.avatar }} />
+                        <Name>{item.name}</Name>
+                        <Bio>{item.bio}</Bio>
+                        <ProfileButton>
+                            <ProfileButtonText>Ver Perfil</ProfileButtonText>
+                        </ProfileButton>
+                    </User>
+                )}
+            />
         </Container>
     );
 };
